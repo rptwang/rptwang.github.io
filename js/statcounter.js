6afb098261a4de9af939d7d14628a36039811f81
rptwang <rwang@RPT-Wang.lan>
2016-08-06 21:33:06 +0100

    u

--- js/statcounter.js
+++
@@ -1,9 +0,0 @@
-var sc_project=10186263; 
-var sc_invisible=0; 
-var sc_security="7989d224"; 
-var sc_text=2; 
-var scJsHost = (("https:" == document.location.protocol) ?
-"https://secure." : "http://www.");
-document.write("<sc"+"ript type='text/javascript' src='" +
-scJsHost+
-"statcounter.com/counter/counter.js'></"+"script>");
